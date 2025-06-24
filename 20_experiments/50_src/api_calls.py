a14c10a2ead9b1b327e8e7caefbbefa1649ffda6
mgrube753 <malteg@online.de>
2025-06-24 18:04:42 +0200

    create exp 1a questions for openai and anthropic; google and deepseek missing

--- 20_experiments/50_src/api_calls.py
+++ 20_experiments/50_src/api_calls.py
@@ -125,7 +125,8 @@ def llm_generation(llm_name, clients, prompt_text, max_tokens, temperature=None)
 
     result = None
     if llm_name == "google":
-        result = gen_with_google(client, prompt_text, model_id, max_tokens, temperature)
+        # result = gen_with_google(client, prompt_text, model_id, max_tokens, temperature)
+        pass
     elif llm_name == "anthropic":
         result = gen_with_anthropic(
             client, prompt_text, model_id, max_tokens, temperature
