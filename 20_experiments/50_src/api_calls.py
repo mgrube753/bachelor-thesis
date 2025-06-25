91c25983299d91046dde8d9a8fead5a55807d419
mgrube753 <malteg@online.de>
2025-06-25 11:12:33 +0200

    complete question set for experiment 1 is finished

--- 20_experiments/50_src/api_calls.py
+++ 20_experiments/50_src/api_calls.py
@@ -127,13 +127,11 @@ def llm_generation(llm_name, clients, prompt_text, max_tokens, temperature=None)
     if llm_name == "google":
         result = gen_with_google(client, prompt_text, model_id, max_tokens, temperature)
     elif llm_name == "anthropic":
-        pass
-        # result = gen_with_anthropic(
-        #     client, prompt_text, model_id, max_tokens, temperature
-        # )
+        result = gen_with_anthropic(
+            client, prompt_text, model_id, max_tokens, temperature
+        )
     elif llm_name == "openai":
-        pass
-        # result = gen_with_openai(client, prompt_text, model_id, max_tokens, temperature)
+        result = gen_with_openai(client, prompt_text, model_id, max_tokens, temperature)
     else:
         print(f"[ERROR] Unknown LLM name '{llm_name}'")
         return None
